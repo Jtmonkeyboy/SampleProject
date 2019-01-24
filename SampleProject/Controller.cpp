4b4474271bf7581452cc8f7a2c3a552d626605f6
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2019-01-24 13:44:46 -0700

    Made a controller

---
+++ SampleProject/Controller.cpp
@@ -0,0 +1,19 @@
+//
+//  Controller.cpp
+//  SampleProject
+//
+//  Created by Runyan, Jacob on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+Controller :: Controller()
+{
+    
+}
+
+void Controller :: start()
+{
+    cout << "This is inside the start method? Cooleroo :D" << endl;
+}
---
+++ SampleProject/Runner.cpp
@@ -0,0 +1,20 @@
+//
+//  Runner.cpp
+//  SampleProject
+//
+//  Created by Runyan, Jacob on 1/24/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+//    Controller app;
+//    app.start();
+    
+    Controller * app = new Controller();
+    app->start();
+    
+    return 0;
+}
